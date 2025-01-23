108f44f060b5e5c1c4a1e6f28b2e7f05b44bd8c6
Shyam Rithul R S <shyamrithulrenju1234@gmail.com>
2025-01-23 11:48:35 +0530

    chnages

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,14 +3,14 @@ import SpaceBackground from "./ThreeCanvas";
 import "./About.css";
 import nethra from "../about.png";
 import logo from "../loho.png";
+import { Link } from "react-router-dom";
 
 const AboutSection = () => {
   const projects = [
     {
       name: "About Me",
       description:
-      "Mecwin Nethra -Remote Monitoring system-IoT Designed and developed Mecwin Nethra, a real-time web application for remote monitoring, asset tracking, and maintenance management. Implemented a multi-role system with secure access and tailored functionality, including a robust ticketing system for efficient issue resolution. Optimized the platform’s UI/UX with modern front-end technologies, ensuring intuitive and responsive interactions across all devices.",
-      image: nethra,
+      "Experienced Front-End Developer specializing in React, with a proven track record of delivering responsive and user-centric web applications. Skilled in creating intuitive interfaces, optimizing performance, and ensuring cross-browser compatibility. Passionate about staying updated with modern web technologies and contributing to innovative projects. Eager to join a dynamic team to build impactful, scalable, and cutting-edge solutions.",
       skills: [
         "HTML",
         "CSS",
@@ -46,15 +46,16 @@ const AboutSection = () => {
 
   return (
     <div className="Pro-container">
-       <div className="Pro_">
-              <div className="Pro_-logo">
-                <img src={logo} alt="Company Logo" />
-              </div>
-              <nav className="Pro_-nav-links">
-                <a href="/">Home</a>
-                <a href="/ContactMe">Contact</a>
-              </nav>
-            </div>
+<div className="project_header">
+        <div className="project_logo">
+          <img src={logo} alt="Company Logo" />
+        </div>
+        <nav className="project_nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/projects">Projects</Link>
+          <Link to="/ContactMe">Contact</Link>
+        </nav>
+      </div>
       <SpaceBackground />
 
       <div className={`Pro-content ${animationClass}`}>
